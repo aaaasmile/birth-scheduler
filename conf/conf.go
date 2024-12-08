446d604959c7eb645849f84467d81bde019ff603
aaaasmile <testhaus@gmx.at>
2024-12-08 17:22:56 +0100

    added custom configuration

--- conf/conf.go
+++ conf/conf.go
@@ -1,13 +1,25 @@
 package conf
 
 import (
+	"log"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
 
+type Relay struct {
+	Mail   string
+	Secret string
+	Host   string
+	User   string
+}
+
 type Config struct {
 	DataFileName string
+	Relay        *Relay
+	EmailTarget  string
 }
 
 var Current = &Config{}
@@ -20,5 +32,26 @@ func ReadConfig(configfile string) (*Config, error) {
 	if _, err := toml.DecodeFile(configfile, &Current); err != nil {
 		return nil, err
 	}
+	if err := readCustomOverrideConfig(Current, configfile); err != nil {
+		return nil, err
+	}
+
+	log.Println("Configuration: ", Current.EmailTarget, Current.Relay)
 	return Current, nil
+}
+
+func readCustomOverrideConfig(Current *Config, configfile string) error {
+	base := path.Base(configfile)
+	dd := path.Dir(configfile)
+	ext := path.Ext(configfile)
+	cf := strings.Replace(base, ext, "_custom.toml", 1)
+	cf_ful := path.Join(dd, cf)
+	log.Println("Check for custom config ", cf_ful)
+	if _, err := os.Stat(cf_ful); err != nil {
+		log.Println("No custom config file found")
+		return nil
+	}
+	log.Println("Custom config file found", cf_ful)
+	_, err := toml.DecodeFile(cf_ful, Current)
+	return err
 }
